d94a52e764e6d3c1c1a843851064592917ad34d4
fedor <lesssa886@gmail.com>
2024-09-08 23:25:51 +0300

    my commit message

--- database/database.py
+++ database/database.py
@@ -1,6 +1,71 @@
-user_dict_template = {
-    'page': 1,
-    'bookmarks': set()
-}
+import sqlite3 as sq
 
-users_db = {}
+def set_page_one(user_id):
+    with sq.connect('users.db') as con:
+        cursor = con.cursor()
+
+        cursor.execute('UPDATE Users SET page = ? WHERE id = ?', (1, user_id))
+
+
+def new_user(user_id):
+    with sq.connect('users.db') as con:
+        cursor = con.cursor()
+
+        cursor.execute('SELECT * FROM Users WHERE id = ?', (user_id,))
+
+        if cursor.fetchall():
+            return
+
+        cursor.execute(f'INSERT INTO Users (id, page) VALUES ({user_id}, 3)')
+
+
+def page_user(user_id):
+    with sq.connect('users.db') as con:
+        cursor = con.cursor()
+
+        cursor.execute('SELECT page FROM Users WHERE id = ?', (user_id,))
+
+        return cursor.fetchall()[0][0]
+
+
+def bookmarks_user(user_id):
+    with sq.connect('users.db') as con:
+        cursor = con.cursor()
+
+        cursor.execute('SELECT * FROM UsersBookmarks WHERE id = ?', (user_id,))
+
+        return set(map(lambda x: x[1], cursor.fetchall()))
+
+
+def change_user_page(user_id, change_page):
+    with sq.connect('users.db') as con:
+        cursor = con.cursor()
+
+        cursor.execute('UPDATE Users SET page = ? WHERE id = ?', (change_page, user_id))
+
+
+def add_user_bookmarks(user_id, page_book):
+    with sq.connect('users.db') as con:
+        cursor = con.cursor()
+
+        cursor.execute('INSERT INTO UsersBookmarks (id, bookmark) VALUES (?, ?)', (user_id, page_book))
+
+
+with sq.connect('users.db') as con:
+    cursor = con.cursor()
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS Users (id INTEGER PRIMARY KEY, page INTEGER)''')
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS UsersBookmarks (id INTEGER, bookmark INTEGER)''')
+
+    cursor.execute('''INSERT INTO Users (id, page) VALUES (12, 2)''')
+
+    con.commit()
+
+
+
+
+
+
+
+
--- handlers_list/user_handlers.py
+++ handlers_list/user_handlers.py
@@ -1,9 +1,12 @@
-from copy import deepcopy
-
 from aiogram import F, Router
 from aiogram.filters import Command, CommandStart
 from aiogram.types import CallbackQuery, Message
-from database.database import user_dict_template, users_db
+from database.database import (new_user,
+                               set_page_one,
+                               page_user,
+                               bookmarks_user,
+                               change_user_page,
+                               add_user_bookmarks)
 from hand_filters.admin_filter import IsDelBookmarkCallbackData, IsDigitCallbackData
 from keyboards_bots.backmarks_kb import (create_bookmarks_keyboard,
                                     create_edit_keyboard)
@@ -16,8 +19,7 @@ router = Router()
 @router.message(CommandStart())
 async def send_start_message(message: Message):
     await message.answer(LEXICON[message.text])
-    if message.from_user.id not in users_db:
-        users_db[message.from_user.id] = deepcopy(user_dict_template)
+    new_user(message.from_user.id)
         
         
 @router.message(Command(commands=['help']))
@@ -27,27 +29,27 @@ async def send_help_message(message: Message):
 
 @router.message(Command(commands=['beginning']))
 async def send_beginning_message(message: Message):
-    users_db[message.from_user.id]['page'] = 1
-    text = book[users_db[message.from_user.id]['page']]
+    set_page_one(message.from_user.id)
+    text = book[page_user(message.from_user.id)]
     await message.answer(
         text=text,
-        reply_markup=create_pagination_keyboard('backward', f'{users_db[message.from_user.id]['page']}/{len(book)}', 'forward')
+        reply_markup=create_pagination_keyboard('backward', f'{page_user(message.from_user.id)}/{len(book)}', 'forward')
         )
 
     
 @router.message(Command(commands=['continue']))
 async def send_continue_message(message: Message):
-    text = book[users_db[message.from_user.id]['page']]
+    text = book[page_user(message.from_user.id)]
     await message.answer(
         text=text,
-        reply_markup=create_pagination_keyboard('backward', f'{users_db[message.from_user.id]['page']}/{len(book)}', 'forward')
+        reply_markup=create_pagination_keyboard('backward', f'{page_user(message.from_user.id)}/{len(book)}', 'forward')
     )
     
     
 @router.message(Command(commands=['bookmarks']))
-async def send_bookmarls_message(message: Message):
-    if users_db[message.from_user.id]['bookmarks']:
-        tt = users_db[message.from_user.id]['bookmarks']
+async def send_bookmarks_message(message: Message):
+    if bookmarks_user(message.from_user.id):
+        tt = bookmarks_user(message.from_user.id)
         await message.answer(
             text=LEXICON[message.text],
             reply_markup=create_bookmarks_keyboard(*tt)
@@ -58,43 +60,42 @@ async def send_bookmarls_message(message: Message):
         
 @router.callback_query(F.data == 'forward')
 async def edit_forward_text(callback: CallbackQuery):
-    if users_db[callback.from_user.id]['page'] < len(book):
-        users_db[callback.from_user.id]['page'] += 1
-        text = book[users_db[callback.from_user.id]['page']]
+    if page_user(callback.from_user.id) < len(book):
+        change_user_page(callback.from_user.id, page_user(callback.from_user.id) + 1)
+        text = book[page_user(callback.from_user.id)]
         await callback.message.edit_text(
             text=text,
-            reply_markup=create_pagination_keyboard('backward', f'{users_db[callback.from_user.id]['page']}/{len(book)}', 'forward')
+            reply_markup=create_pagination_keyboard('backward', f'{page_user(callback.from_user.id)}/{len(book)}', 'forward')
         )
     await callback.answer()
     
 
 @router.callback_query(F.data == 'backward')
 async def edit_backward_text(callback: CallbackQuery):
-    if users_db[callback.from_user.id]['page'] > 1:
-        users_db[callback.from_user.id]['page'] -= 1
-        text = book[users_db[callback.from_user.id]['page']]
+    if page_user(callback.from_user.id) > 1:
+        change_user_page(callback.from_user.id, page_user(callback.from_user.id) - 1)
+        text = book[page_user(callback.from_user.id)]
         await callback.message.edit_text(
             text=text,
-            reply_markup=create_pagination_keyboard('backward', f'{users_db[callback.from_user.id]['page']}/{len(book)}', 'forward')
+            reply_markup=create_pagination_keyboard('backward', f'{page_user(callback.from_user.id)}/{len(book)}', 'forward')
         )
     await callback.answer()
     
     
 @router.callback_query(lambda x: '/' in x.data and x.data.replace('/', '').isdigit()) 
 async def add_bookmarks(callback: CallbackQuery):
-    users_db[callback.from_user.id]['bookmarks'].add(
-        users_db[callback.from_user.id]['page']
-    ) 
-    await callback.answer(text=f'Страница {users_db[callback.from_user.id]['page']} успешно добавлена в закладки!')  
+    add_user_bookmarks(callback.from_user.id, page_user(callback.from_user.id))
+
+    await callback.answer(text=f'Страница {page_user(callback.from_user.id)} успешно добавлена в закладки!')
     
     
 @router.callback_query(IsDigitCallbackData())
 async def open_bookmarks(callback: CallbackQuery):
-    users_db[callback.from_user.id]['page'] = int(callback.data)
+    change_user_page(callback.from_user.id, int(callback.data))
     text = book[int(callback.data)]
     await callback.message.edit_text(
         text=text,
-        reply_markup=create_pagination_keyboard('backward', f'{users_db[callback.from_user.id]['page']}/{len(book)}', 'forward')
+        reply_markup=create_pagination_keyboard('backward', f'{page_user(callback.from_user.id)}/{len(book)}', 'forward')
     )
     
     
@@ -102,7 +103,7 @@ async def open_bookmarks(callback: CallbackQuery):
 async def edit_bookmarks(callback: CallbackQuery):
     await callback.message.edit_text(
         text=LEXICON['edit_bookmarks'],
-        reply_markup=create_edit_keyboard(*users_db[callback.from_user.id]['bookmarks'])
+        reply_markup=create_edit_keyboard(*bookmarks_user(callback.from_user.id))
     ) 
     
     
@@ -113,13 +114,13 @@ async def cancel_bookmarks(callback: CallbackQuery):
     
 @router.callback_query(IsDelBookmarkCallbackData())
 async def del_bookmarks(callback: CallbackQuery):
-    users_db[callback.from_user.id]['bookmarks'].remove(
+    bookmarks_user(callback.from_user.id).remove(
         int(callback.data[:-3])
     )
-    if users_db[callback.from_user.id]['bookmarks']:
+    if bookmarks_user(callback.from_user.id):
         await callback.message.edit_text(
             text=LEXICON['edit_bookmarks'],
-            reply_markup=create_edit_keyboard(*users_db[callback.from_user.id]['bookmarks'])
+            reply_markup=create_edit_keyboard(*bookmarks_user(callback.from_user.id))
         )
     else:
         await callback.message.edit_text(
